5e6c755b9bbea57446e41f61918ee1551f7af3dc
Sergey Vlasov <sigprof@gmail.com>
2022-06-23 11:04:32 +0300

    Add a simple flake which uses Nixpkgs for testing

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Test flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages = flake-utils.lib.flattenTree {
+        default = pkgs.hello;
+      };
+    });
+}
