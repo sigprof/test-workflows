077a17cb4c041ce870acb3455316ac00262bbf39
Sergey Vlasov <sigprof@gmail.com>
2022-06-26 16:29:33 +0300

    flake: Add `lib.ciOutputs` to generate matrices for CI
    Using `jq` to massage the output of `nix flake show --json` into the
    form required by the CI is too complicated; use the Nix code to generate
    the required JSON instead.  This also solves the problem of missing
    platform info for `nixosConfigurations` in the `nix flake show` output
    (the Nix code can check `v.config.nixpkgs.system` and put the `host`
    entry under the appropriate system).

--- flake.nix
+++ flake.nix
@@ -18,6 +18,9 @@
         inherit (pkgs) hello firefox jq;
         default = pkgs.hello;
       });
+      checks = self.lib.removeNullValues {
+        default-package = self.packages.${system}.default or null;
+      };
     })
     // (let
       system = flake-utils.lib.system.x86_64-linux;
@@ -34,6 +37,28 @@
             }
           ];
         };
+      };
+      lib = let
+        inherit (nixpkgs.lib) isAttrs genAttrs attrNames filterAttrs;
+        inherit (flake-utils.lib) defaultSystems;
+        ciMatrix = name: attrs:
+          if (!isAttrs attrs) || (attrs == {})
+          then null
+          else {
+            ${name} = attrNames attrs;
+          };
+        ciPackagesFor = system: removeAttrs (self.packages.${system} or {}) ["default"];
+        ciHostsFor = system:
+          filterAttrs (n: v: v.config.nixpkgs.system == system) (self.nixosConfigurations or {});
+      in {
+        removeNullValues = filterAttrs (n: v: v != null);
+        ciOutputs = genAttrs defaultSystems (system: {
+          flake = self.lib.removeNullValues {
+            checks = ciMatrix "check" (self.checks.${system} or {});
+            packages = ciMatrix "package" (ciPackagesFor system);
+            hosts = ciMatrix "host" (ciHostsFor system);
+          };
+        });
       };
     });
 }
