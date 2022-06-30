514c53582d47e4ca24a2d47478bf8d0d644cb592
Sergey Vlasov <sigprof@gmail.com>
2022-06-30 12:04:32 +0300

    ci: Add proper package filtering to `nur.nix`

--- nur.nix
+++ nur.nix
@@ -1,5 +1,21 @@
 {pkgs ? import <nixpkgs> {}}: let
   inherit (pkgs) system;
+
+  # Load this flake using `edolstra/flake-compat`.  Note that the resulting
+  # flake refers to the locked `nixpkgs` revision, and therefore is not
+  # suitable for the NUR use case, but some parts from that flake can still be
+  # used without causing a download of that `nixpkgs` revision.
+  selfFlakeCompat =
+    (import (let
+      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+      inherit (lock.nodes.flake-compat.locked) owner repo rev narHash;
+    in
+      fetchTarball {
+        url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+        sha256 = narHash;
+      }) {src = ./.;})
+    .defaultNix;
+
   self = {
     NUR = true;
     inputs = {
@@ -12,11 +28,15 @@
     };
     lib = import ./lib self.inputs;
 
-    # FIXME: use `pkgs.callPackage`, then filter to usable packages only
-    nurPackages.${system} = import ./pkgs {
-      inherit (self) inputs;
-      inherit pkgs;
-    };
+    nurPackages.${system} = let
+      inherit (selfFlakeCompat.inputs.flake-utils.lib) filterPackages flattenTree;
+    in
+      filterPackages system (flattenTree (
+        pkgs.callPackage ./pkgs {
+          inherit (self) inputs;
+          inherit pkgs;
+        }
+      ));
   };
 in
   self
