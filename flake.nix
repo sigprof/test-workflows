2a36a2003f6fa81c5a2236e8c23d3d7b179f1caf
Sergey Vlasov <sigprof@gmail.com>
2022-06-30 12:04:05 +0300

    flake: Add `edolstra/flake-compat` to inputs

--- flake.nix
+++ flake.nix
@@ -4,12 +4,16 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     flake-utils.url = "github:numtide/flake-utils";
+
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-compat.flake = false;
   };
 
   outputs = inputs @ {
     self,
     nixpkgs,
     flake-utils,
+    ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
